89fa5defa71d452c43a420bc69f4a4b30cc6211f
Jaspreet Pahal <jaspreet.pahal@gmail.com>
2020-11-22 12:20:30 +0530

    Initial Commit

---
+++ ProjectCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectCalculator
+//
+//  Created by Jaspreet Pahal on 11/22/20.
+//  Copyright © 2020 Jaspreet Pahal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
